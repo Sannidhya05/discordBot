040ba5891f33b9fc4b5cf42230272d0b5ece38c7
Sannidhya Jaiswal <jaiswalsannidhya@gmail.com>
2024-11-20 01:49:39 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ BOT_TOKEN = os.getenv('TOKEN')
 
 # Bot setup
 intents = discord.Intents.default()
+intents.members = True
 intents.messages = True
 intents.guilds = True
 intents.message_content = True
@@ -21,10 +22,49 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'Logged in as {bot.user}!')
 
+# Event : Errors
+@bot.event
+async def on_ready():
+    print(f"Bot is ready as {bot.user}")
+
+# Event : Member joining
+@bot.event
+async def on_member_join(member):
+    role = discord.utils.get(member.guild.roles, name="Member")  # Replace with the role name
+    channel = bot.get_channel(1208757573885624355) # Replace with your channel id
+    if role:
+        await member.add_roles(role)
+    if channel:
+        await channel.send(f"Welcome {member.mention}! You have been given the {role.name} role.")
+
+# Event : Member leaving
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1208757573885624355) # Replace with your channel id
+    if channel:
+        await channel.send(f"Sadly {member.mention}  left us :(")
+
 # Command: Ping
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+# Command: Commands
+@bot.command()
+async def commands(ctx):
+    await ctx.send('''Commands:
+ping - to ping the bot.
+greet {name} - to greet someone.
+commands - display all the commands.
+join - to make it hop onto the vc you are in.
+play {url} - makes it play the yt url you pasted.
+stop - stops playing music.
+leave - leaves the vc.''')
+
+# Command : Say
+@bot.command()
+async def say(ctx, content):
+    await ctx.send(f'{content}')
 
 # Command: Join a voice channel
 @bot.command()
